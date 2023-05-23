a8c9bfc7a7da3d485ea32309644cdf5f18eb812d
Kelvim Kauam <kelvimkauam@gmail.com>
2023-05-23 11:12:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "decription": "Marvel anuncia um novo filme do homem de ferro, confira",
+        "photoCover": "https://resizing.flixster.com/jTU4RAjnwcZAconup5QZkH2Emb8=/206x305/v2/https://flxt.tmsimg.com/assets/p170620_p_v8_az.jpg"
+    },
+    {
+        "id": "2",
+        "title": "FLOPOU!! Tentaram lacrar e não lucraram.",
+        "decription": "bla bla bla",
+        "photoCover": "https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_FMjpg_UX1000_.jpg"
+    }
+]
